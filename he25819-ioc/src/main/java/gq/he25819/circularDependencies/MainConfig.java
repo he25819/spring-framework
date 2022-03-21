259a35a28ac3889a0ad91b782a43150ee24a540b
xinweihe <he25819@163.com>
2022-03-22 04:23:29 +0800

    剩余代码

--- he25819-ioc/src/main/java/gq/he25819/circularDependencies/MainConfig.java
+++ he25819-ioc/src/main/java/gq/he25819/circularDependencies/MainConfig.java
@@ -2,8 +2,11 @@ package gq.he25819.circularDependencies;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan("gq.he25819.circularDependencies")
+//@ImportResource(value = {"classpath:beans/beans.xml"})
+@EnableAspectJAutoProxy
 public class MainConfig {
 }
